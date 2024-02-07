8989717fae5a42c068f4b3e45dc34cac6e6a60eb
DomagojFrontend <domagojfrontend@gmail.com>
2024-02-07 17:38:17 +0100

    Fixing <ContentDoc /> error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: true,
   modules: ['@nuxt/content', '@nuxt/ui', '@vueuse/nuxt'],
   content: {
     highlight: {
       theme: 'github-dark'
     }
   },
-  routeRules: {
-    '/documentation': {prerender: true, redirect: '/documentation/get_started/about' },
+  // routeRules: {
+    // '/documentation/**': { prerender: true },
+    // '/documentation': {prerender: true, redirect: '/documentation/get_started/about' },
     // '/documentation/**/**': { prerender: true },
-  }
+  // }
 })
